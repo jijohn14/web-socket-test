1715efbfad3b116b86667045178bc1c66723c6ee
JJ <jijohn@microsoft.com>
2021-10-07 15:08:52 -0700

    Make the message string

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ var WebSocketServer = require('ws').Server
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
-        ws.send(message);
+        ws.send('' + message);
     });
  });
 EOS
